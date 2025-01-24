68b7f05b7d569bbd54834613bcb70b7581df2148
Riley Coover <rileywcoover@gmail.com>
2025-01-23 22:53:48 -0800

    Initial Commit

---
+++ AdditiveSynthClean/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AdditiveSynthClean
+//
+//  Created by Riley on 1/23/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
